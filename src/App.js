8ab608255eead8fb6b591dcf081b7b75bff8bd9d
Amel Mejri <web.amel.m@gmail.com>
2019-12-12 22:03:47 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Chrono from './component/chrono'
+
+function App() {
+  return (
+    <div className="App">
+     <Chrono/>
+    </div>
+  );
+}
+
+export default App;
